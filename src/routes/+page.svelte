ad344a5723edd10899c3b503ff45053b413e723d
VicSchbt <victoire.schubert@gmail.com>
2024-02-23 10:29:35 +0100

    chores: init style variables

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="scss">
+	@import url('https://fonts.googleapis.com/css2?family=Space+Mono:wght@700&display=swap');
+
+	$gt-mobile: 375px;
+
+	$primary-strong-cyan: #26c0ab;
+
+	$neutral-very-dark-cyan: #00494d;
+	$neutral-dark-grayish-cyan: #5e7a7d;
+	$neutral-grayish-cyan: #7f9c9f;
+	$neutral-light-grayish-cyan: #c5e4e7;
+	$neutral-very-light-grayish-cyan: #f4fafa;
+	$white: #fff;
+
+	$form-input-font-size: 24px;
+
+	* {
+		font-family: 'Space Mono', monospace;
+		font-weight: 700;
+		font-style: normal;
+	}
+</style>
